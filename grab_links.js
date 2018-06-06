76b3fda64d759e2756d4721da1621cc26cc2c085
root <2062320169@qq.com>
2018-06-06 11:53:52 +0800

    first

---
+++ grab_links.js
@@ -0,0 +1,43 @@
+console.log('open all BR started');
+
+
+
+window.addEventListener('click', function (evt) {
+	if (evt.detail === 3) {     
+		chrome.runtime.sendMessage({greeting:grab_links()}, function(response) {
+			console.log(response.farewell);
+		});
+		add_border()
+
+	}
+});
+
+
+function add_border(){
+	for (var i = 1; i < 11; i++) {
+		document.getElementById(i)
+		.getElementsByClassName('t')[0]
+		.getElementsByTagName('a')[0]
+		.style.border = "1px solid red"
+	}
+}
+
+
+function grab_links(){
+	links=[]
+	for (var i = 1; i < 11; i++) {
+		link=document.getElementById(i).getElementsByClassName('t')[0].getElementsByTagName('a')[0].href
+
+		links = links.concat(link);
+	}
+	console.log(links)
+
+	links_json=JSON.stringify(links);
+
+	return links_json;
+}
+
+
+
+
+
